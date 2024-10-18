ec7e72a1a30ee7357a87a8cae635d567ee1fd2b8
seuriseuljjeok <heeseul09@naver.com>
2024-10-18 20:49:29 +0900

    [Add] StringLiterals 추가 (#1)

---
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/Global/Literal/StringLiterals.swift
@@ -0,0 +1,18 @@
+//
+//  StringLiterals.swift
+//  DATEROAD-SwiftUI
+//
+//  Created by 윤희슬 on 10/18/24.
+//
+
+import Foundation
+
+enum LOGIN {
+        
+    static let KAKAO_LOGIN = "카카오 로그인"
+    
+    static let APPLE_LOGIN = "Apple ID로 로그인"
+    
+    static let PRIVACY_POLICY = "개인정보처리방침"
+    
+}
