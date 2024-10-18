91d2a836dd9ea0f1b053beb10ee927113fdfba4d
seuriseuljjeok <heeseul09@naver.com>
2024-10-18 19:14:19 +0900

    [Setting] 프로젝트 생성

---
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/DATEROAD_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DATEROAD_SwiftUIApp.swift
+//  DATEROAD-SwiftUI
+//
+//  Created by 윤희슬 on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DATEROAD_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
