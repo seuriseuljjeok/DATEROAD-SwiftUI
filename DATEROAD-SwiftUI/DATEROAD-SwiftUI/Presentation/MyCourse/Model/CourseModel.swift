cda644bc4ee92ef4746d36746cc809822057aaf2
seuriseuljjeok <heeseul09@naver.com>
2024-10-22 23:47:24 +0900

    [Add] 이미지 & 타이틀 구성 컴포넌트 추가 (#5)

---
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/MyCourse/Model/CourseModel.swift
@@ -0,0 +1,8 @@
+//
+//  CourseModel.swift
+//  DATEROAD-SwiftUI
+//
+//  Created by 윤희슬 on 10/22/24.
+//
+
+import Foundation
---
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/MyCourse/View/MyRegisteredCourse.swift
@@ -0,0 +1,18 @@
+//
+//  MyRegisteredCourse.swift
+//  DATEROAD-SwiftUI
+//
+//  Created by 윤희슬 on 10/22/24.
+//
+
+import SwiftUI
+
+struct MyRegisteredCourse: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyRegisteredCourse()
+}
