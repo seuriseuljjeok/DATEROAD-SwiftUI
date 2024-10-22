ad4943794d434f1b3b1e237a527eb1ece64f4841
seuriseuljjeok <heeseul09@naver.com>
2024-10-22 23:48:10 +0900

    [Add] Text 기본 속성 지정 extension 추가 (#5)

--- DATEROAD-SwiftUI/DATEROAD-SwiftUI/Global/Extension/Text+.swift
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Global/Extension/Text+.swift
@@ -5,4 +5,23 @@
 //  Created by 윤희슬 on 10/22/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension Text {
+    
+    func setText(maxWidth: CGFloat? = .infinity,
+                 maxHeight: CGFloat? = nil,
+                 alignment: Alignment = .leading,
+                 font: FontName,
+                 textColor: Color = .black000,
+                 padding: EdgeInsets = EdgeInsets(.zero),
+                 lineLimit: Int = 1
+    ) -> some View {
+        self.frame(maxWidth: maxWidth, maxHeight: maxHeight, alignment: alignment)
+            .lineLimit(lineLimit)
+            .font(.suit(font))
+            .foregroundStyle(textColor)
+            .padding(padding)
+    }
+    
+}
