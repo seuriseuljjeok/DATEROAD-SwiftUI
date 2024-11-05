9847fc54fd116dae468a5b2b8cc10bec15ba2d8b
seuriseuljjeok <heeseul09@naver.com>
2024-11-05 19:21:24 +0900

    [Feat] 성향 태그 뷰 UI 구현 (#5)

--- DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/Profile/View/ProfileView.swift
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/Profile/View/ProfileView.swift
@@ -77,3 +77,76 @@ struct NicknameView: View {
 
     }
 }
+
+struct ChipView: View {
+    
+    @Binding var tagCount: Int
+    
+    @Binding var isValidTagCount: Bool
+    
+    @State private var selectedTags: [Bool] = Array(repeating: false, count: TendencyTag.allCases.count)
+    
+    @State private var chipData: [any Chipable] = TendencyTag.allCases.map { $0.tag }
+    
+    @State private var height: CGFloat = 106 // ChipView의 높이를 저장하기 위한 상태 변수
+
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ChipLayoutView(
+                tagData: $chipData,
+                verticalSpacing: 8,
+                horizontalSpacing: 7
+            ) { index in
+                let data = chipData[index]
+                Button(action: {
+                    withTransaction(Transaction(animation: nil)) {
+                        if selectedTags[index] {
+                            selectedTags[index] = false
+                            tagCount -= 1
+                        } else {
+                            if tagCount < 3 {
+                                selectedTags[index] = true
+                                tagCount += 1
+                            }
+                        }
+                    }
+                    checkTagCount()
+                }) {
+                    HStack(spacing: 2) {
+                        Image(data.tagIcon)
+                            .padding(.leading, 16)
+                        Text(data.tagTitle)
+                            .font(.suit(.body_med_13))
+                            .padding(.trailing, 12)
+                            .foregroundColor(selectedTags[index] ? .white000 : .black000)
+                    }
+                }
+                .frame(height: 30)
+                .background(selectedTags[index] ? .purple600 : .gray100) // 선택된 상태에 따라 배경색 변경
+                .cornerRadius(15)
+            }
+            .onAppear {
+                // ChipLayoutView의 높이를 ChipView의 높이로 설정
+                let totalHeight = geometry.size.height
+                print("Total Height: \(totalHeight)")
+                DispatchQueue.main.async {
+                    // ChipView의 높이를 ChipLayoutView의 총 높이에 맞게 설정
+                    self.height = totalHeight
+                }
+            }
+        }
+        .frame(height: height)
+    }
+    
+    func checkTagCount() {
+        isValidTagCount = tagCount > 0 && tagCount < 4
+    }
+    
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
