4f4d38812fc99901a62f48a7c1508f4aedae7aca
seuriseuljjeok <heeseul09@naver.com>
2024-11-09 20:10:31 +0900

    [Add] '홈' 다가오는 데이트 일정 모델 추가 (#7)

---
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/Home/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  DATEROAD-SwiftUI
+//
+//  Created by 윤희슬 on 11/9/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
