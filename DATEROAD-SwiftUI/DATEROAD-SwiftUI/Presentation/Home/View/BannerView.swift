ac7714e222dfcaac556b7e20b6ff18ef576333ec
seuriseuljjeok <heeseul09@naver.com>
2024-11-13 22:01:35 +0900

    [Add] 배너 모델 추가 (#7)

---
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/Home/View/BannerView.swift
@@ -0,0 +1,18 @@
+//
+//  BannerView.swift
+//  DATEROAD-SwiftUI
+//
+//  Created by 윤희슬 on 11/13/24.
+//
+
+import SwiftUI
+
+struct BannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BannerView()
+}
---
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/Home/View/NewDateCourseView.swift
@@ -0,0 +1,18 @@
+//
+//  NewDateCourseView.swift
+//  DATEROAD-SwiftUI
+//
+//  Created by 윤희슬 on 11/13/24.
+//
+
+import SwiftUI
+
+struct NewDateCourseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewDateCourseView()
+}
