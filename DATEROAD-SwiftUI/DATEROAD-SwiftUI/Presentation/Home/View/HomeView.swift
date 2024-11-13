95279892cc201fb636a63947bc71c4a746968813
seuriseuljjeok <heeseul09@naver.com>
2024-11-13 22:03:48 +0900

    [Feat] 홈 UI 구현 (#7)

--- DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/Home/View/HomeView.swift
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/Home/View/HomeView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct HomeView: View {
+    
     var body: some View {
-        ZStack {
-            Color(.purple600).ignoresSafeArea()
+        ZStack(alignment: .top) {
+            Color(.purple600)
+                .frame(width: .infinity, height: 400)
+                .ignoresSafeArea()
+            GeometryReader { geometry in
+                let width = geometry.size.width // 좌우 여백 제외
+                let height = width * 0.385
+                ScrollView(.vertical, showsIndicators: false) {
+                    LazyVStack(spacing: 0) {
+                        TopUserInfoView()
+                        HotDateCourseView().offset(x: 0, y: -21)
+                        BannerView(width: width, height: height)
+                        NewDateCourseView()
+                    }
+                    .frame(maxWidth: .infinity, alignment: .top)
+                    .background(.white000)
+                }
+                .frame(maxWidth: .infinity)
+            }
         }
     }
 }
