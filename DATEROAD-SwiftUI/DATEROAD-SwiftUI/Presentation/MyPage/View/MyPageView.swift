c97d3d0b92e0923cd665637c08f94bfabe23928a
seuriseuljjeok <heeseul09@naver.com>
2024-11-18 18:15:05 +0900

    [Chore] 코드 정렬 (#9)

--- DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/MyPage/View/MyPageView.swift
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/MyPage/View/MyPageView.swift
@@ -26,35 +26,49 @@ struct MyPageView: View {
                     UserInfoView()
                         .padding(EdgeInsets(top: 0, leading: 16, bottom: 16, trailing: 16))
                         .background(.gray100, ignoresSafeAreaEdges: .all)
-                        .clipShape(RoundedCornerShape(corners: [.bottomLeft, .bottomRight], radius: 14))
+                        .clipShape(
+                            RoundedCornerShape(
+                                corners: [.bottomLeft, .bottomRight],
+                                radius: 14
+                            )
+                        )
                         .zIndex(1)
-                    MyPageSectionView(showLogoutAlert: $showLogoutAlert,showWithDrawalAlert: $showWithDrawalAlert, isLogout: $isLogout)
-                        .zIndex(0)
-                        .padding(.top, -16)
+                    MyPageSectionView(
+                        showLogoutAlert: $showLogoutAlert,
+                        showWithDrawalAlert: $showWithDrawalAlert,
+                        isLogout: $isLogout
+                    )
+                    .zIndex(0)
+                    .padding(.top, -16)
                     Spacer()
                 }
-                .customNavigationBar(hasLeftIcon: false, hasLeftTitle: true, title: NAVBAR.MYPAGE)
-                .customAlert(showAlert: isLogout ? $showLogoutAlert : $showWithDrawalAlert,
-                             alertType: isLogout ? .DoubleButtonWithSingleTitle : .DoubleButtonWithDoubleTitle,
-                             primaryTitle: isLogout ? ALERT.LOGOUT_PRIMARY_TITLE : ALERT.WITHDRAWAL_PRIMARY_TITLE,
-                             secondaryTitle: isLogout ? nil : ALERT.WITHDRAWAL_SECONDARY_TITLE,
-                             rightButtonTitle: isLogout ? ALERT.LOGOUT : ALERT.CANCEL,
-                             leftButtonTitle: isLogout ? ALERT.CANCEL : ALERT.WITHDRAWAL,
-                             rightButtonAction: {
-                    // TODO: - 추후 서버 통신으로 변경
-                    if isLogout {
-                        print("로그아웃!")
-                    } else {
-                        showWithDrawalAlert = false
-                    }
-                },
-                             leftButtonAction: {
-                    if isLogout {
-                        showLogoutAlert = false
-                    } else {
-                        print("탈퇴!")
-                    }
-                })
+                .customNavigationBar(
+                    hasLeftIcon: false,
+                    hasLeftTitle: true,
+                    title: NAVBAR.MYPAGE
+                )
+                .customAlert(
+                    showAlert: isLogout ? $showLogoutAlert : $showWithDrawalAlert,
+                    alertType: isLogout ? .DoubleButtonWithSingleTitle : .DoubleButtonWithDoubleTitle,
+                    primaryTitle: isLogout ? ALERT.LOGOUT_PRIMARY_TITLE : ALERT.WITHDRAWAL_PRIMARY_TITLE,
+                    secondaryTitle: isLogout ? nil : ALERT.WITHDRAWAL_SECONDARY_TITLE,
+                    rightButtonTitle: isLogout ? ALERT.LOGOUT : ALERT.CANCEL,
+                    leftButtonTitle: isLogout ? ALERT.CANCEL : ALERT.WITHDRAWAL,
+                    rightButtonAction: {
+                        // TODO: - 추후 서버 통신으로 변경
+                        if isLogout {
+                            print("로그아웃!")
+                        } else {
+                            showWithDrawalAlert = false
+                        }
+                    },
+                    leftButtonAction: {
+                        if isLogout {
+                            showLogoutAlert = false
+                        } else {
+                            print("탈퇴!")
+                        }
+                    })
             }
         }
     }
