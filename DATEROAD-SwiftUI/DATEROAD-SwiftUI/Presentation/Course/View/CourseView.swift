7ecb1e5107a0b1b7a5d906d30e22ed0731b089f5
seuriseuljjeok <heeseul09@naver.com>
2024-11-24 22:41:06 +0900

    [Feat] 코스 리스트뷰 UI 구현 (#11)

--- DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/Course/View/CourseView.swift
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/Course/View/CourseView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct CourseView: View {
+    
+    private let priceFilter: [String] = [PRICETAG.UNDER30K, PRICETAG.UNDER50K, PRICETAG.UNDER100K, PRICETAG.OVER100K]
+    
     var body: some View {
         NavigationView {
             ZStack(alignment: .top) {
                 Color(.white000)
+                VStack {
                     CourseFilteringView
+                    CourseListView()
+                }
             }
             .customNavigationBar(
                 hasRightIcon: true,
@@ -75,6 +81,94 @@ struct CourseView: View {
             }
         .padding(EdgeInsets(top: 9, leading: 0, bottom: 20, trailing: 0))
     }
+    
+    private struct CourseListView: View {
+        
+        @State var courseListData: [CourseListModel] = CourseListModel.dummy
+        
+        let columns = [GridItem(.flexible(), spacing: 15), GridItem(.flexible(), spacing: 15)]
+        
+        var body: some View {
+            GeometryReader { geometry in
+                let width = (geometry.size.width - 47) / 2
+                let height = geometry.size.height
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach($courseListData, id: \.courseId) { data in
+                            CourseListItem(courseData: data, width: width, height: height)
+                        }
+                    }
+                }
+                .padding(.horizontal, 16)
+            }
+        }
+    }
+    
+    private struct CourseListItem: View {
+        
+        @Binding var courseData: CourseListModel
+        
+        @State var width: CGFloat
+
+        @State var height: CGFloat
+        
+        var body: some View {
+            VStack(spacing: 0) {
+                ZStack(alignment: .bottomLeading) {
+                    Image(courseData.thumbnail)
+                        .resizable()
+                        .frame(width: width, height: width * 0.8536)
+                        .clipShape(RoundedRectangle(cornerRadius: 14))
+                    ImageWithTextView(
+                        text: String(courseData.like),
+                        font: .body_bold_13,
+                        imagePadding: EdgeInsets(top: 2, leading: 10, bottom: 2, trailing: 0),
+                        radius: 12,
+                        viewPadding: EdgeInsets(top: 0, leading: 5, bottom: 5, trailing: 0))
+                }
+                Text(courseData.location)
+                    .setText(
+                        alignment: .leading,
+                        font: .body_med_13,
+                        textColor: .gray400,
+                        padding: EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0)
+                    )
+                Text(courseData.title)
+                    .frame(maxWidth: .infinity, minHeight: 43, alignment: .topLeading)
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0))
+                    .font(.suit(.body_bold_17))
+                    .lineLimit(2)
+                    .foregroundStyle(.black000)
+                HStack(spacing: 18) {
+                    ImageWithTextView(text: courseData.cost.priceRangeTag(),
+                                      textPadding: EdgeInsets(.zero),
+                                      font: .body_med_13,
+                                      textColor: .gray400,
+                                      image: "ic_won",
+                                      imagePadding: EdgeInsets(top: 1.5, leading: 0, bottom: 1.5, trailing: 0),
+                                      backgroundColor: .white000,
+                                      radius: 20,
+                                      viewPadding: EdgeInsets(.zero))
+                    ImageWithTextView(text: courseData.time.formatFloatTime() + "시간",
+                                      textPadding: EdgeInsets(.zero),
+                                      font: .body_med_13,
+                                      textColor: .gray400,
+                                      image: "ic_clock",
+                                      imagePadding: EdgeInsets(top: 1.5, leading: 0, bottom: 1.5, trailing: 0),
+                                      backgroundColor: .white000,
+                                      radius: 20,
+                                      viewPadding: EdgeInsets(.zero))
+                }
+                .frame(width: width, alignment: .leading)
+            }
+            .frame(width: width, height: width * 1.378)
+            .onTapGesture {
+                // TODO: - 코스 상세로 이동
+            }
+        }
+    }
+    
 }
 
 struct CourseView_Preview: PreviewProvider {
