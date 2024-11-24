09c03ccaaf3c5e1c8d1f162ecb63790ee1896e6c
seuriseuljjeok <heeseul09@naver.com>
2024-11-24 22:40:26 +0900

    [Add] 가격 및 지역 변환 extension 추가 (#11)

--- DATEROAD-SwiftUI/DATEROAD-SwiftUI/Global/Extension/Float+.swift
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Global/Extension/Float+.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension Float {
+    
+    func formatFloatTime() -> String {
+        if self.truncatingRemainder(dividingBy: 1) == 0 {
+            return String(format: "%.0f", self)
+        } else {
+            return String(self)
+        }
+    }
+    
+}
