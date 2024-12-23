7542056fa6e5ad24cd7978f16a4c79c9cb9661f9
seuriseuljjeok <heeseul09@naver.com>
2024-12-23 17:52:42 +0900

    [Add] 포인트 내역 모델 추가 (#17)

---
+++ DATEROAD-SwiftUI/DATEROAD-SwiftUI/Presentation/Point/View/PointView.swift
@@ -0,0 +1,18 @@
+//
+//  PointView.swift
+//  DATEROAD-SwiftUI
+//
+//  Created by 윤희슬 on 12/23/24.
+//
+
+import SwiftUI
+
+struct PointView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PointView()
+}
